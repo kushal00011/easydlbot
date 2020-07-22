b7102bfcdc6e92d9ea552d54f1cad05cf1924349
kushal00011 <67371292+kushal00011@users.noreply.github.com>
2020-07-22 08:16:33 +0530

    config.json

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
     "token": " 1217903855:AAHxCUZFxAYowKx3qu6qIxmWGgDIHNY-tYA",
-    "joinchat": " http://t.me/Kushaldlbot", 
+    "joinchat": " http://t.me/kushaldlbot", 
     "id": " 689361533 "
 }
